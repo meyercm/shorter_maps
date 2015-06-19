7e6b90a8ec53129cdb73af0f797aa7f8d4084d97
Andrea Leopardi <an.leopardi@gmail.com>
2015-06-19 11:57:14 +0200

    Remove some boilerplate comments from mix.exs

--- mix.exs
+++ mix.exs
@@ -1,31 +1,21 @@
 defmodule ShortMaps.Mixfile do
   use Mix.Project
 
+  @version "1.0.0-alpha"
+
   def project do
     [app: :short_maps,
-     version: "0.0.1",
+     version: @version,
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: []]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
   end
