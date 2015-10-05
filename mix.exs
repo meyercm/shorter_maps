97fa8035f05e008c0c4000d6f187c1cc1a5d4e21
Andrea Leopardi <an.leopardi@gmail.com>
2015-10-04 21:42:37 -0500

    Make the package releasable

--- mix.exs
+++ mix.exs
@@ -1,7 +1,8 @@
 defmodule ShortMaps.Mixfile do
   use Mix.Project
 
-  @version "1.0.0-alpha"
+  @version "0.1.0"
+  @repo_url "https://github.com/whatyouhide/short_maps"
 
   def project do
     [app: :short_maps,
@@ -9,14 +10,26 @@ defmodule ShortMaps.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     # Hex
+     package: hex_package,
+     description: "Implementation of a ~m sigil for ES6-like maps in Elixir",
+     # Docs
+     name: "ShortMaps",
+     docs: [source_ref: "v#{@version}", main: "ShortMaps", source_url: @repo_url]]
   end
 
   def application do
     [applications: []]
   end
 
+  defp hex_package do
+    [maintainers: ["Andrea Leopardi"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => @repo_url}]
+  end
+
   defp deps do
-    []
+    [{:ex_doc, "~> 0.10", only: :docs}]
   end
 end
